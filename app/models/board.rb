8b2ddae1ebe9af39184355b548c8ab2fd49d821a
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-18 21:46:42 +0000

    fixed typo on board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -8,5 +8,5 @@
 #  updated_at :datetime         not null
 #
 class Board < ApplicationRecord
-  validates(:name, { :present => true , :uniqueness => true })
+  validates(:name, { :presence => true , :uniqueness => true })
 end
